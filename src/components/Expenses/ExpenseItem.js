c7288d9d42e5ecc96ddecb3cc5c456eb63cedfcb
Kacper Falecki <kfalecki17@gmail.com>
2023-01-09 02:04:02 +0100

    Add form, ExpenseForm, NewExpense

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,17 +1,27 @@
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
-import React from "react";
+import React, { useState } from "react";
 import './ExpenseItem.css';
 
 const ExpenseItem = (expenseInfo) => {
+    
+    const [title, setTitle] = useState(expenseInfo.title);
+
+
+
+    const clickHandler = () => {
+        setTitle('Updated!');
+        console.log(title);
+    };
 
     return (
         <Card className='expense-item'>
             <ExpenseDate date={expenseInfo.date}/>
             <div className='expense-item__description'>
-                <h2>{expenseInfo.title}</h2>
+                <h2>{title}</h2>
                 <div className='expense-item__price'>{expenseInfo.amount}</div>
             </div>
+            <button onClick={clickHandler}>Change title</button>
         </Card>
     );
 }
